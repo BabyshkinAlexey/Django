17f1cba6fc28fd29c399b38316defc815eb16b6a
Alexey <mr.alexv2000@mail.ru>
2024-04-13 00:05:23 +0500

    Перестройка

--- seminars/seminar_1/myproject/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(reqvest):
-    return HttpResponse('Hello, world!')
-
-def about(reqvest):
-    return HttpResponse('About, us')
-
